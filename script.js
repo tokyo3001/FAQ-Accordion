a3454fd69a3840bbfa6ac5fdd91ed3bc1c525076
tokyo3001 <84511419+tokyo3001@users.noreply.github.com>
2021-07-22 16:29:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+var acc = document.getElementsByClassName("question");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var answer = this.nextElementSibling;
+    if (answer.style.maxHeight) {
+      answer.style.maxHeight = null;
+    } else {
+      answer.style.maxHeight = answer.scrollHeight + "px";
+    } 
+  });
+}
